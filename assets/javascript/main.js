f0319f27ff03cbddc1084bfa40f9ebc49333286f
kellidragon <dragonkelli@gmail.com>
2019-07-12 16:54:55 -0700

    contact form submitting

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,12 +4,34 @@ $(document).ready(function(){
      particlesJS.load('particles-js', 'package.json');
     })
     
-    
+    AOS.init();
 
-  AOS.init();
 
     $(".submit-btn").on("click", function(event){
         event.preventDefault();
-    })
+        $('#myModal').modal('show')
+        // $('#myModal').modal({ show: false})
+        $('.validate').val("");
+     
+    function validateForm() {
+          var isValid = true;
+          $('.validate').each(function () {
+              if ($(this).val() === "") {
+                  isValid = true;
+              }
+          });
+          console.log(isValid)
+          return isValid;
+      }
+
+      if (validateForm() == true) {
+
+        console.log("Thank you for contacting me")
+    }else {
+           
+            
+      alert("Please fill out all fields before submitting.")
+    }
+  })
     
     })
